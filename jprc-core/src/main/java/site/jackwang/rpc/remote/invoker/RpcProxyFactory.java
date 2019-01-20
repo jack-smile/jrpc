f24265dea1e0d455c1a41aedb6330cb5c96b77a0
wangjie <513330940@qq.com>
2019-01-20 15:26:15 +0800

    优化调用方式，改为jdk动态代理

---
+++ jprc-core/src/main/java/site/jackwang/rpc/remote/invoker/RpcProxyFactory.java
@@ -0,0 +1,20 @@
+package site.jackwang.rpc.remote.invoker;
+
+import site.jackwang.rpc.remote.net.impl.netty.client.NettyClient;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * 生成代理对象
+ *
+ * @author wangjie<http://www.jackwang.site/>
+ * @date 2019/1/20
+ */
+public final class RpcProxyFactory {
+    public static <T> T getProxy(Class<T> proxyClass, NettyClient client) {
+        InvocationHandler handler = new RpcInvocationHandler(proxyClass, client);
+
+        return (T) Proxy.newProxyInstance(handler.getClass().getClassLoader(), new Class[]{proxyClass}, handler);
+    }
+}
--- jrpc-samples/jrpc-sample-frameless/jrpc-sample-frameless-client/src/main/java/site/jackwang/rpc/JRpcClient.java
+++ jrpc-samples/jrpc-sample-frameless/jrpc-sample-frameless-client/src/main/java/site/jackwang/rpc/JRpcClient.java
@@ -1,16 +1,23 @@
 package site.jackwang.rpc;
 
-import site.jackwang.rpc.proxy.CalculatorServiceProxy;
+import site.jackwang.rpc.remote.invoker.RpcProxyFactory;
+import site.jackwang.rpc.remote.net.impl.netty.client.NettyClient;
 import site.jackwang.rpc.serialize.impl.ProtostuffSerializer;
+import site.jackwang.rpc.service.CalculatorService;
 
 /**
  * @author wangjie<http://www.jackwang.site/>
  * @date 2019/1/17
  */
 public class JRpcClient {
-    public static void main(String[] args) {
-        CalculatorServiceProxy calculatorServiceProxy = new CalculatorServiceProxy("127.0.0.1", 8888, new ProtostuffSerializer());
-        System.out.println(calculatorServiceProxy.add(1.0f, 2.0f));
-        calculatorServiceProxy.stop();
+    public static void main(String[] args) throws InterruptedException {
+//        CalculatorServiceProxy calculatorServiceProxy = new CalculatorServiceProxy("127.0.0.1", 8888, new ProtostuffSerializer());
+//        System.out.println(calculatorServiceProxy.add(1.0f, 2.0f));
+//        calculatorServiceProxy.stop();
+
+        NettyClient client = new NettyClient();
+        client.init("127.0.0.1", 8888, new ProtostuffSerializer());
+        CalculatorService calculatorService = RpcProxyFactory.getProxy(CalculatorService.class, client);
+        System.out.println(calculatorService.add(1.0f, 2.0f));
     }
 }
